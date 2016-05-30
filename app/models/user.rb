1b31224a1d4f4ba7f1c51b67b9a403febf36b982
Charlie Hoang <charlie.b.hoang@gmail.com>
2016-05-30 14:41:15 -0400

    basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.*[a-z\d\-]+\.[a-z]+\z/i
+  has_secure_password
+  validates :name, presence: true, 
+                  length: { maximum: 50 }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+                      length: { minimum: 6 }
+
+  def email=(value)
+    # some dbs use case sensitive indicies; this normalizes email
+    super(value.downcase) 
+  end
+end
